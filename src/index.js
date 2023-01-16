a08fef257a0ac96ac46662dd298d5c61afd04829
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-01-16 19:24:57 +0200

    added the initial setup for the router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+      <Routes>
+        <Route path="/" element="" />
+        <Route path="/about" element="" />
+        <Route path="/technologies" element="" />
+        <Route path="/projects" element="" />
+        <Route path="/project/:id" element="" />
+        <Route path="*" element="" />
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
