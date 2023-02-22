4b28832db5c2b37ceee09613ad6db0865f16074a
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-22 19:01:30 +0200

    added the about-section initial structure

---
+++ src/components/About/About.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <section className="about">
+        <div className="section-container">
+          <div className="section-title">About</div>
+          <p className="about-text">
+            I've worked with arange of technologies in the web development
+            world. From Back End to Design. I’m always aiming to write code
+            using the latest best practices in software development and suitable
+            design patterns based on logic.
+          </p>
+          <div className="timeline">
+            <div className="timeline-year">2017</div>
+            <div className="timeline-text">
+              Started my journey as a computer science student.
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import App from "./App";
 import Home from "./components/Home/Home";
 import Projects from "./components/Projects/Projects";
 import Technologies from "./components/Technologies/Technologies";
+import About from "./components/About/About";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,7 +17,7 @@ root.render(
       <App />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element="" />
+        <Route path="/about" element={<About />} />
         <Route path="/technologies" element={<Technologies />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/project/:id" element="" />
