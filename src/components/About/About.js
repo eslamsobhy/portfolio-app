92882b6cef85483f67f37220f99d28831b613ee6
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-02-22 20:51:01 +0200

    styled the about section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,9 @@ import React from "react";
 // data
 import { timelineData } from "../../constants/constants";
 
+// styling
+import "./about.css";
+
 const About = () => {
   return (
     <>
@@ -15,16 +18,16 @@ const About = () => {
             using the latest best practices in software development and suitable
             design patterns based on logic.
           </p>
-          <div className="timeline-center">
-            {timelineData.map((timeline, index) => {
-              return (
-                <div className="timeline" key={index}>
-                  <div className="timeline-year">{timeline.year}</div>
-                  <div className="timeline-text">{timeline.text}</div>
-                </div>
-              );
-            })}
-          </div>
+        </div>
+        <div className="timeline-center">
+          {timelineData.map((timeline, index) => {
+            return (
+              <div className="timeline" key={index}>
+                <div className="timeline-year">{timeline.year}</div>
+                <div className="timeline-text">{timeline.text}</div>
+              </div>
+            );
+          })}
         </div>
       </section>
     </>
