4fb608a541643f8c1727de4b175408a60a6ad95a
Eslam Sobhy <eslamsobhy206@gmail.com>
2023-03-15 21:08:19 +0200

    fixed the not-scrolling to top bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // main components
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header/header";
 import Navigation from "./components/RespNav/Navigation";
 
 function App() {
   const [isResNavOpen, setIsResNavOpen] = useState(false);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [isResNavOpen]);
 
   return (
     <div className="App">
